adebaaef45f1ac2fb9f06f425ef10da6cab416d2
Shazia Afreen <166709231+Shafreen123@users.noreply.github.com>
2025-05-30 00:49:35 +0530

    tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,16 @@ module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}", 
   ],
-  theme: {
-    extend: {},
+   theme: {
+    extend: {
+      fontFamily: {
+        // Default sans font for general use (optional)
+        sans: ['Poppins', 'sans-serif'],
+
+        // Formal serif font for your quote or headings
+        formal: ['"Playfair Display"', 'serif'],
+      },
+    },
   },
   plugins: [],
 }
